0cf83dc31050a2e76fb88516a23f8286874aa68e
Vladimir Ponomarenko <v2ponomarenko@gmail.com>
2023-06-07 22:12:08 +0300

    вечерний кодинг

--- Move.cpp
+++ Move.cpp
@@ -2,25 +2,44 @@
 
 namespace chess{
 
-Move::Move(int x1, int y1, int x2, int y2){
-    start = Position(x1, y1);
-    end = Position(x2, y2);
+Move::Move(int x1, int y1, int x2, int y2) 
+    : start(x1, y1), end(x2, y2), pawnTransformPiece(ChessPieceType::Blank, Color::Black),
+    capturedPiece(ChessPieceType::Blank, Color::Black){
     doneEnPassant = false;
     pawnTransform = false;
-    pawnTransformPiece = ChessPieceType::Blank;
     takingIsDone = false;
-    capturedPiece = ChessPieceType::Blank;
+    shortCastlingLost = false;
+    longCastlingLost = false;
+    localMovesWithoutMistakes = 0;
+    evaluate = 0;
 }
 
-Move::Move(Position, Position){
-
+Move::Move(Position point_start, Position point_end) 
+    : start(point_start), end(point_end),
+    pawnTransformPiece(ChessPieceType::Blank, Color::Black),
+    capturedPiece(ChessPieceType::Blank, Color::Black){
+    doneEnPassant = false;
+    pawnTransform = false;
+    takingIsDone = false;
+    shortCastlingLost = false;
+    longCastlingLost = false;
+    localMovesWithoutMistakes = 0;
+    evaluate = 0;
 }
 
-Move::Move(const Move&){
-
+Move::Move(const Move& mv) : start(mv.start), end(mv.end), 
+    pawnTransformPiece(mv.pawnTransformPiece),capturedPiece(mv.capturedPiece){
+    doneEnPassant = mv.doneEnPassant;
+    pawnTransform = mv.pawnTransform;
+    takingIsDone = mv.takingIsDone;
+    shortCastlingLost = mv.shortCastlingLost;
+    longCastlingLost = mv.longCastlingLost;
+    localMovesWithoutMistakes = mv.localMovesWithoutMistakes;
+    evaluate = mv.evaluate;
 }
 
 std::string Move::toString(){
-
+    return start.toString() + "-" + end.toString();
 }
+
 }
--- chess-engine.cpp
+++ chess-engine.cpp
@@ -2,7 +2,9 @@
 
 namespace chess{
 
+constexpr int DEFAULT_DEPTH = 6;
 
+int cal
 
 
 }
